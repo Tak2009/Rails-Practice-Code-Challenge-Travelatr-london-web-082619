d542011d1b25b8d70fc4647980b0a6738647a3d4
github username <github email address>
2019-09-24 21:51:56 +0100

    updated

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,22 +1,41 @@
 class DestinationsController < ApplicationController
   def index
+    @destinations = Destination.all
   end
 
   def new
+    @destination = Destination.new
   end
 
   def show
+    @destination = Destination.find(params[:id])
   end
 
   def create
+    @destination = Destination.create(destination_params)
+    if @destination.valid?
+      redirect_to destination_path(@destination)
+    else
+      render :new
+    end
   end
 
   def edit
+    @destination = Destination.find(params[:id])
   end
 
   def update
+    @destination = Destination.find(params[:id])
+    @destination.update(destination_params)
+    if @destination.valid?
+      redirect_to destination_path(@destination)
+    else
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    Destination.destroy(params[:id])
+    redirect_to destinations_path
   end
 end
